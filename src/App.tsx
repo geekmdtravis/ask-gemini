8a06eb3470f94c58eaa2b2129773473b032254d0
geekmdtravis <travis@geekmd.io>
2025-07-10 20:57:52 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Hello, world!</h1>
+      <p className="text-red-500">This is a test.</p>
+    </div>
+  )
+}
+
+export default App
