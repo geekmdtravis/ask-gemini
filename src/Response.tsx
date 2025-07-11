b9d04189402e8cab881a1aa198ed580c464dbdec
geekmdtravis <travis@geekmd.io>
2025-07-10 22:34:56 -0700

    Improved markdown rendering

--- src/Response.tsx
+++ src/Response.tsx
@@ -16,7 +16,7 @@ const Response: React.FC<ResponseProps> = ({
     <div className="relative flex-grow">
       <div
         id="response"
-        className={`prose prose-sm p-2.5 w-full text-white bg-gray-700 rounded-lg border border-gray-600 min-h-[100px] ${
+        className={`prose prose-sm prose-invert p-2.5 w-full text-white bg-gray-700 rounded-lg border border-gray-600 min-h-[100px] ${
           loading ? "blur-sm" : ""
         }`}
       >
@@ -24,36 +24,17 @@ const Response: React.FC<ResponseProps> = ({
           <ReactMarkdown
             remarkPlugins={[remarkGfm]}
             components={{
-              h1: ({ node, ...props }) => (
-                <h4 className="text-lg font-bold" {...props} />
-              ),
-              h2: ({ node, ...props }) => (
-                <h5 className="text-base font-bold" {...props} />
-              ),
-              h3: ({ node, ...props }) => (
-                <h6 className="text-sm font-bold" {...props} />
-              ),
-              h4: ({ node, ...props }) => (
-                <h6 className="text-sm font-bold" {...props} />
-              ),
-              h5: ({ node, ...props }) => (
-                <h6 className="text-sm font-bold" {...props} />
-              ),
-              h6: ({ node, ...props }) => (
-                <h6 className="text-sm font-bold" {...props} />
-              ),
-              a: ({ node, ...props }) => (
-                <a
-                  className="text-blue-400 hover:underline"
-                  {...props}
-                  target="_blank"
-                />
+              pre: ({ node, ...props }) => (
+                <pre className="bg-gray-800 rounded-md p-2" {...props} />
               ),
               code: ({ node, ...props }) => (
-                <code className="text-sm bg-gray-800 p-1 rounded" {...props} />
+                <code className="bg-gray-800 rounded-md px-1" {...props} />
               ),
-              pre: ({ node, ...props }) => (
-                <pre className="text-sm bg-gray-800 p-2 rounded" {...props} />
+              ol: ({ node, ...props }) => (
+                <ol className="list-decimal pl-5" {...props} />
+              ),
+              ul: ({ node, ...props }) => (
+                <ul className="list-disc pl-5" {...props} />
               ),
             }}
           >
